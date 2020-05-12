9842b9cf52ac0bd0c8fc43f11780f597f8871d29
André Euzébio <andre.r.euzebio@gmail.com>
2020-05-11 22:04:02 -0300

    Configuring routes

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
-
+const routes = require('./routes');
 const server = express();
-const videos = require('./data');
 
 server.use(express.static('public'))
 
@@ -14,39 +13,7 @@ nunjucks.configure("views", {
     noCache: true
 })
 
-server.get("/", function(req, res) {
-    const about = {
-        avatar_url: "https://avatars3.githubusercontent.com/u/40874927?s=460&v=4",
-        name: "André Euzébio",
-        role: "Desenvolvedor de Sistemas",
-        description: 'Formado em Sistemas de Informações. Colaborador da <a href="https://senior.com.br" target="_blank">Senior Sistemas</a>',
-        links: [
-            { name: "Github" , url: "https://github.com/andre-euz"},
-            { name: "Instagram" , url: "https://www.instagram.com/andre_rdg/?hl=pt-br"},
-            { name: "Linkedin" , url: "https://www.linkedin.com/in/andreeuzebio/"}
-        ]
 
-    }
-    return res.render("about", {about});
-})
-
-server.get("/portfolio", function(req, res) {
-    return res.render("portfolio", { items: videos });
-})
-
-server.get("/video", function(req, res) {
-    const id = req.query.id;
-
-    const video = videos.find(function(video){
-        return video.id == id;        
-    })
-
-    if (!video) {
-        return res.send("Video not found!");
-    }
-    return res.render("video", { item: video })
-
-})
 
 server.listen(5000, function() {
     console.log("server is running");
