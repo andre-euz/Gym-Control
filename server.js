5426e67df20e6733684e29094db98781bf76b60d
André Euzébio <andre.r.euzebio@gmail.com>
2020-05-11 22:29:59 -0300

    Working on new structure of project

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const routes = require('./routes');
+
 const server = express();
 
-server.use(express.static('public'))
+server.use(express.static('public'));
+server.use(routes);
 
 server.set("view engine", "njk");
 
@@ -12,8 +14,6 @@ nunjucks.configure("views", {
     autoescape: false,
     noCache: true
 })
-
-
 
 server.listen(5000, function() {
     console.log("server is running");
