ac4aca49b464952206d6af9c7eb744874a266d05
André Euzébio <andre.r.euzebio@gmail.com>
2020-07-17 16:37:25 -0300

    Structuring dataof instructors

--- instructors.js
+++ instructors.js
@@ -3,7 +3,7 @@ const data = require('./data.json');
 // create
 exports.post = function(req, res){
     // req.query
-    //req.body
+    // req.body
 
     const keys = Object.keys(req.body);
 
@@ -13,10 +13,21 @@ exports.post = function(req, res){
             return res.send('Please, fill all fields.');
     }
 
-    req.body.birth = Date.parse(req.body.birth);    
-    req.body.created_at = Date.now();
+    let {avatar_url, birth, name, services, gender} = req.body;
 
-    data.instructors.push(req.body);
+    birth = Date.parse(birth);    
+    const created_at = Date.now();
+    const id = Number(data.instructors.length + 1);
+
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at
+    });
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
         if (err) return res.send("Write file error!");
