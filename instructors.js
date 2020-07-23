be77904f1f74005410583e446eeea43f6d08d9ee
André Euzébio <andre.r.euzebio@gmail.com>
2020-07-23 15:16:31 -0300

    Using another way to format date of field created_at with INTL lib

--- instructors.js
+++ instructors.js
@@ -16,7 +16,8 @@ exports.show = function(req, res) {
         ...foundInstructor,
         age: age(foundInstructor.birth),
         services: foundInstructor.services.split(","),
-        created_at: new Date(foundInstructor.created_at).toLocaleDateString()
+        //created_at: new Date(foundInstructor.created_at).toLocaleDateString()
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at)
     }
 
     return res.render("instructors/show",{ instructor });
