18f4b41ee49009d92f4d6aca9fdba0b3fe876661
André Euzébio <andre.r.euzebio@gmail.com>
2020-07-23 16:47:30 -0300

    Working with date types

--- utils.js
+++ utils.js
@@ -5,9 +5,19 @@ module.exports = {
 
         let age = today.getFullYear() - birthDate.getFullYear();
         const month = today.getMonth() - birthDate.getMonth();
-        if (month < 0 || (month == 0 && today.getDate() < birthDate.getDate(timestamp))) {
+        if (month < 0 || 
+           (month == 0 && 
+            today.getDate() < birthDate.getDate(timestamp))) {
             age -= 1; 
         }  
         return age;         
+    },
+    date: function(timestamp) {
+        const date = new Date(timestamp);
+        const year = date.getUTCFullYear();
+        const month = `0${date.getUTCMonth() + 1}`.slice(-2);
+        const day = `0${date.getUTCDate()}`.slice(-2);
+
+        return `${year}-${month}-${day}`;
     }    
 }
